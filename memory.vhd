38cee0f0637b435c23f12e76af2c1851a2d0b615
Dipanshu Barnwal <58032366+bdip635@users.noreply.github.com>
2020-03-27 13:39:45 +0530

    Update memory.vhd

--- memory.vhd
+++ memory.vhd
@@ -1,18 +1,47 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-type portout1 is array(0 to 15) of std_logic_vector(7 downto 0);
 
 entity memory is
 
 port( address: in std_logic_vector(7 downto 0);
       data_in: in std_logic_vector(7 downto 0);
 		w: in std_logic;
-		clock: in std_logic;
-		portout: out portout1;
-		portin: in portout1;
+		clock,reset: in std_logic;
 		data_out: out std_logic_vector(7 downto 0);
-		reset: in std_logic
-		);
+      port_in_00: in std_logic_vector(7 downto 0);   
+        port_in_01: in std_logic_vector(7 downto 0);  
+        port_in_02: in std_logic_vector(7 downto 0);    
+        port_in_03: in std_logic_vector(7 downto 0);   
+        port_in_04: in std_logic_vector(7 downto 0);   
+        port_in_05: in std_logic_vector(7 downto 0);   
+        port_in_06: in std_logic_vector(7 downto 0);   
+        port_in_07: in std_logic_vector(7 downto 0);   
+        port_in_08: in std_logic_vector(7 downto 0);   
+        port_in_09: in std_logic_vector(7 downto 0);  
+        port_in_10: in std_logic_vector(7 downto 0);   
+        port_in_11: in std_logic_vector(7 downto 0);   
+        port_in_12: in std_logic_vector(7 downto 0);   
+        port_in_13: in std_logic_vector(7 downto 0);   
+        port_in_14: in std_logic_vector(7 downto 0);    
+        port_in_15: in std_logic_vector(7 downto 0);  
+         
+        port_out_00: out std_logic_vector(7 downto 0);   
+        port_out_01: out std_logic_vector(7 downto 0);  
+        port_out_02: out std_logic_vector(7 downto 0);    
+        port_out_03: out std_logic_vector(7 downto 0);   
+        port_out_04: out std_logic_vector(7 downto 0);   
+        port_out_05: out std_logic_vector(7 downto 0);   
+        port_out_06: out std_logic_vector(7 downto 0);   
+        port_out_07: out std_logic_vector(7 downto 0);   
+        port_out_08: out std_logic_vector(7 downto 0);   
+        port_out_09: out std_logic_vector(7 downto 0);  
+        port_out_10: out std_logic_vector(7 downto 0);   
+        port_out_11: out std_logic_vector(7 downto 0);   
+        port_out_12: out std_logic_vector(7 downto 0);   
+        port_out_13: out std_logic_vector(7 downto 0);   
+        port_out_14: out std_logic_vector(7 downto 0);    
+        port_out_15: out std_logic_vector(7 downto 0)  
+        		);
 end memory;
 
 architecture memory1 of memory is
@@ -27,7 +56,7 @@ end component;
 
 component rw is
 port( w: in std_logic;
-      address: in std_logic_address(7 downto 0);
+      address: in std_logic_vector(7 downto 0);
 		clock: in std_logic;
 		data_in: in std_logic_vector(7 downto 0);
 		data_out: out std_logic_vector(7 downto 0)
@@ -40,7 +69,23 @@ port( address: in std_logic_vector(7 downto 0);
 		data_in: in std_logic_vector(7 downto 0);
 		w: in std_logic;
 		reset: in std_logic;
-		portout: out portout1);
+      port_out_00: out std_logic_vector(7 downto 0);   
+        port_out_01: out std_logic_vector(7 downto 0);  
+        port_out_02: out std_logic_vector(7 downto 0);    
+        port_out_03: out std_logic_vector(7 downto 0);   
+        port_out_04: out std_logic_vector(7 downto 0);   
+        port_out_05: out std_logic_vector(7 downto 0);   
+        port_out_06: out std_logic_vector(7 downto 0);   
+        port_out_07: out std_logic_vector(7 downto 0);   
+        port_out_08: out std_logic_vector(7 downto 0);   
+        port_out_09: out std_logic_vector(7 downto 0);  
+        port_out_10: out std_logic_vector(7 downto 0);   
+        port_out_11: out std_logic_vector(7 downto 0);   
+        port_out_12: out std_logic_vector(7 downto 0);   
+        port_out_13: out std_logic_vector(7 downto 0);   
+        port_out_14: out std_logic_vector(7 downto 0);    
+        port_out_15: out std_logic_vector(7 downto 0)  
+             );
 end component;
 
 begin
@@ -57,44 +102,43 @@ x3: outports port map( address => address,
 							  clock => clock,
 							  data_in => data_in,
 							  reset => reset,
-							  portout => portout);
-							  
---multiplexing input and output
-MUX1: process(address, rom_data_out, rw_data_out, portin)
-		begin
-			if ( (to_integer(unsigned(address)) >= 0) and
-			(to_integer(unsigned(address)) <= 127)) then
-			data_out <= rom_data_out;
-
-			elsif ( (to_integer(unsigned(address)) >= 128) and
-			(to_integer(unsigned(address)) <= 223)) then
-			data_out <= rw_data_out;
-			
-			elsif (address = x"F0") then data_out <= portin(0);
-			elsif (address = x"F1") then data_out <= portin(1);
-			elsif (address = x"F2") then data_out <= portin(2);
-			elsif (address = x"F3") then data_out <= portin(3);
-			elsif (address = x"F4") then data_out <= portin(4);
-			elsif (address = x"F5") then data_out <= portin(5);
-			elsif (address = x"F6") then data_out <= portin(6);
-			elsif (address = x"F7") then data_out <= portin(7);
-			elsif (address = x"F8") then data_out <= portin(8);
-			elsif (address = x"F9") then data_out <= portin(9);
-			elsif (address = x"FA") then data_out <= portin(10);
-			elsif (address = x"FB") then data_out <= portin(11);
-			elsif (address = x"FC") then data_out <= portin(12);
-			elsif (address = x"FD") then data_out <= portin(13);
-			elsif (address = x"FE") then data_out <= portin(14);
-			elsif (address = x"FF") then data_out <= portin1(15);
-			else data_out <= x"00";
-		end if;
-		end process;
+                          port_out_00 => port_out_00,  
+                              port_out_01 => port_out_01,  
+                              port_out_02 => port_out_02,  
+                              port_out_03 => port_out_03,   
+                              port_out_04 => port_out_04,  
+                              port_out_05 => port_out_05,  
+                              port_out_06 => port_out_06,  
+                              port_out_07 => port_out_07,  
+                              port_out_08 => port_out_08,  
+                              port_out_09 => port_out_09,  
+                              port_out_10 => port_out_10,  
+                              port_out_11 => port_out_11,  
+                              port_out_12 => port_out_12,  
+                              port_out_13 => port_out_13,  
+                              port_out_14 => port_out_14,   
+                              port_out_15 => port_out_15
+                              );
+							  		
+data_out <= rom_data_out when address < x"80" else  
+                    rw_data_out when address < x"E0" else  
+                            port_in_00 when address = x"F0" else  
+                            port_in_01 when address = x"F1" else  
+                            port_in_02 when address = x"F2" else  
+                            port_in_03 when address = x"F3" else  
+                            port_in_04 when address = x"F4" else  
+                            port_in_05 when address = x"F5" else  
+                            port_in_06 when address = x"F6" else  
+                            port_in_07 when address = x"F7" else  
+                            port_in_08 when address = x"F8" else  
+                            port_in_09 when address = x"F9" else  
+                            port_in_10 when address = x"FA" else  
+                            port_in_11 when address = x"FB" else  
+                            port_in_12 when address = x"FC" else  
+                            port_in_13 when address = x"FD" else  
+                            port_in_14 when address = x"FE" else  
+                            port_in_15 when address = x"FF" else  
+                            x"00";  
 
 		
 end memory1;
-
-							  
-						 
-
-						 
-
